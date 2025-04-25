80f124a3ddccbbbd0cfcc819bfbcb18a44ce00b9
Vyacheslav chopenko <syavchik260119901@gmail.com>
2025-04-26 01:50:57 +0300

    Improve modal and header

--- src/js/handlers.js
+++ src/js/handlers.js
@@ -1,7 +1,11 @@
 // Функції, які передаються колбеками в addEventListner
-import { refs } from "./refs";
-import { saveThemeToLocalStorage } from "./storage";
-const { body } = refs;
+import { refs } from './refs';
+import { saveThemeToLocalStorage } from './storage';
+const { body, openModalMenuBtn } = refs;
+import {
+  closeModalMenu,
+  openModalMenu,
+} from './modal';
 
 // перемикання тем
 export function handleChangeTheme() {
@@ -13,3 +17,39 @@ export function handleChangeTheme() {
     saveThemeToLocalStorage('theme-light');
   }
 }
+
+// функція для клавіші
+export function handleKeyDown(ev) {
+  if (ev.key === 'Escape') {
+    closeModalMenu();
+  }
+}
+
+export function handleModalClick(ev) {
+  const closeMenuBtn = ev.target.closest('.menu-close-btn');
+  const navLink = ev.target.closest('.menu-nav-link');
+  const orderLink = ev.target.closest('.menu-order-link');
+  console.log(orderLink);
+  console.log(navLinks);
+  
+
+  if (ev.target === ev.currentTarget) return;
+
+  if (closeMenuBtn) {
+    closeModalMenu();
+    return;
+  } else if (orderLink) {
+    closeModalMenu();
+    return;
+  } else if (navLink) {
+    closeModalMenu();
+    return;
+  }
+}
+
+export function handleOpenMenu() {
+  if (openModalMenuBtn) {
+    openModalMenu();
+  }
+}
+
--- src/js/modal.js
+++ src/js/modal.js
@@ -1,1 +1,21 @@
 //Описана робота модалки - відкриття закриття і все що з модалкою повʼязано
+import {
+  handleKeyDown,
+  handleModalClick,
+} from './handlers';
+import { refs } from './refs';
+const { modalMenu } = refs;
+
+export function openModalMenu() {
+  modalMenu.classList.remove('is-close');
+  modalMenu.classList.add('is-open');
+  modalMenu.addEventListener('click', handleModalClick);
+  window.addEventListener('keydown', handleKeyDown);
+}
+
+export function closeModalMenu() {
+  modalMenu.classList.remove('is-open');
+  modalMenu.classList.add('is-close');
+  modalMenu.removeEventListener('click', handleModalClick);
+  window.removeEventListener('keydown', handleKeyDown);
+}
