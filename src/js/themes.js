fef132988e001ddfce91cef4f8dd809b572c6ee3
Vyacheslav chopenko <syavchik260119901@gmail.com>
2025-04-25 17:02:02 +0300

    style: Update footer and header

--- src/js/themes.js
+++ src/js/themes.js
@@ -1,1 +1,17 @@
 // логіка роботи тем
+ import { handleChangeTheme } from './handlers';
+import { refs } from './refs';
+const { themeToggleButton, body } = refs;
+import { loadThemeFromLocalStorage } from './storage';
+
+themeToggleButton.addEventListener('click', handleChangeTheme);
+
+export function applySavedTheme() {
+  const savedTheme = loadThemeFromLocalStorage();
+  if (savedTheme) {
+    body.classList.remove('theme-light', 'theme-dark');
+    body.classList.add(savedTheme);
+  } else {
+    body.classList.add('theme-light');
+  }
+}
