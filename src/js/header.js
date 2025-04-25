053e9ce58be61f8eb9da02de3e04408a3695f74e
Vyacheslav chopenko <syavchik260119901@gmail.com>
2025-04-25 10:14:13 +0300

    Add theme toggle functionality

--- src/js/header.js
+++ src/js/header.js
@@ -1,1 +1,6 @@
 // основна логіка роботи секції
+import { handleChangeTheme } from './handlers';
+import { refs } from './refs';
+const { themeToggleButton } = refs;
+
+themeToggleButton.addEventListener('click', handleChangeTheme);
