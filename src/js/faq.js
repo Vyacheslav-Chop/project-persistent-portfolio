d6bca861132ba5c9e237c3dd96d93e9edb2c76f2
Svitlana <missis.chuvaeva@ukr.net>
2025-04-26 23:27:54 +0300

    html/css faq are ready

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,3 +1,12 @@
 // основна логіка роботи секції
-import Accordion from 'accordion-js';
-import 'accordion-js/dist/accordion.min.css';
+// import Accordion from 'accordion-js';
+// import 'accordion-js/dist/accordion.min.css';
+// const faqArrowBtn = document.querySelectorAll('.faq-arrow-btn');
+// const question = document.querySelectorAll('.faq-question');
+// question.addEventListener('click', handleArrowClick);
+// function handleArrowClick() {}
+
+// new Accordion('.faq-accordion-container', {
+//   openOnInit: [0], // відкриває перший елемент
+//   showMultiple: false, // забороняє відкривати кілька одночасно
+// });
