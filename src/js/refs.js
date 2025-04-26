4f3effa77520e671405fadb31053d6a03d4b7f3f
Vyacheslav chopenko <syavchik260119901@gmail.com>
2025-04-26 23:20:33 +0300

    fixed click in header

--- src/js/refs.js
+++ src/js/refs.js
@@ -4,5 +4,4 @@ export const refs = {
   themeToggleButton: document.querySelector('.toggle-theme-btn'),
   openModalMenuBtn: document.querySelector('.burger-menu-btn'),
   modalMenu: document.querySelector('.menu'),
-  themeTextBox: document.querySelector('.theme-switcher'),
 };
