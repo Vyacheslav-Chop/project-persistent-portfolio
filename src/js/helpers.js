94ee68f6097b04a1d29adfcfb31ec8e87c6d8df0
Vyacheslav chopenko <syavchik260119901@gmail.com>
2025-04-27 15:33:03 +0300

    feat: smooth scroll and blur on page

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -1,1 +1,29 @@
 //Допоміжні функції
+
+function removeFocusFromElements() {
+  const interactiveElements = document.querySelectorAll('a, button');
+  interactiveElements.forEach(interactiveElement => interactiveElement.blur());
+}
+
+export function removeFocus() {
+  const interactiveElements = document.querySelectorAll('a, button');
+  interactiveElements.forEach(interactiveElement =>
+    interactiveElement.addEventListener('click', removeFocusFromElements)
+  );
+}
+
+function smoothScroll(ev) {
+  ev.preventDefault();
+  const target = document.querySelector(ev.currentTarget.getAttribute('href'));
+  if (target) {
+    target.scrollIntoView({ behavior: 'smooth' });
+  }
+
+  ev.currentTarget.blur();
+}
+
+export function scrollToView() {
+  document.querySelectorAll('a[href^="#"]').forEach(link => {
+    link.addEventListener('click', smoothScroll);
+  });
+}
