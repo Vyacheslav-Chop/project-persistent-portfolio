bb281115e90690d55a345239552450d21972bb8c
Svitlana <missis.chuvaeva@ukr.net>
2025-04-28 16:21:23 +0300

    scrollButton

--- src/main.js
+++ src/main.js
@@ -4,3 +4,26 @@ import './js/my-projects';
 import './js/faq';
 import './js/reviews';
 import './js/work-together';
+
+// scroll to the top
+const goTopBtn = document.querySelector('.go-top');
+
+goTopBtn.addEventListener('click', goTop);
+window.addEventListener('scroll', trackScroll);
+
+function trackScroll() {
+  const offset = window.pageYOffset;
+  const coords = document.documentElement.clientHeight;
+  if (offset > coords) {
+    goTopBtn.classList.add('go-top--show');
+  } else {
+    goTopBtn.classList.remove('go-top--show');
+  }
+}
+
+function goTop() {
+  if (window.pageYOffset > 0) {
+    window.scrollBy(0, -15);
+    setTimeout(goTop, 0);
+  }
+}
