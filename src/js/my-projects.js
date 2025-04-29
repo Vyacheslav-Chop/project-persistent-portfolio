ab3c1872ac15389e2b67c63c930fed4e8f78a499
DimaM83 <150599187+DimaM83@users.noreply.github.com>
2025-04-29 15:25:59 +0200

    refactor: переписал отрисовку проектов через map для полной генерации разметки

--- src/js/my-projects.js
+++ src/js/my-projects.js
@@ -11,62 +11,101 @@ document.addEventListener('DOMContentLoaded', () => {
     function renderProjects() {
         const list = document.querySelector('.projects-list');
 
-        list.innerHTML = '';
+        //         list.innerHTML = '';
 
-        const visible = projects.slice(0, visibleProjects);
+        //         const visible = projects.slice(0, visibleProjects);
 
-        visible.forEach(project => {
-            const li = document.createElement('li');
-            li.classList.add('projects-item');
+        //         visible.forEach(project => {
+        //             const li = document.createElement('li');
+        //             li.classList.add('projects-item');
 
-            li.innerHTML = `
-    <picture>
-    <source 
-        media="(min-width: 1280px)" 
-        srcset="${project.img.desktop['1x']} 1x, ${project.img.desktop['2x']
-                } 2x"
-    />
-    <source 
-        media="(min-width: 768px)" 
-        srcset="${project.img.tablet['1x']} 1x, ${project.img.tablet['2x']} 2x"
-    />
-    <source 
-        media="(max-width: 767px)" 
-        srcset="${project.img.mobile['1x']} 1x, ${project.img.mobile['2x']} 2x"
-    />
-    <img 
-        class="projects-image"
-        src="${project.img.mobile['1x']}" 
-        alt="${project.title}" 
-        width="320" 
-        height="180"
-    />
-    </picture>
+        //             li.innerHTML = `
+        //     <picture>
+        //     <source 
+        //         media="(min-width: 1280px)" 
+        //         srcset="${project.img.desktop['1x']} 1x, ${project.img.desktop['2x']
+        //                 } 2x"
+        //     />
+        //     <source 
+        //         media="(min-width: 768px)" 
+        //         srcset="${project.img.tablet['1x']} 1x, ${project.img.tablet['2x']} 2x"
+        //     />
+        //     <source 
+        //         media="(max-width: 767px)" 
+        //         srcset="${project.img.mobile['1x']} 1x, ${project.img.mobile['2x']} 2x"
+        //     />
+        //     <img 
+        //         class="projects-image"
+        //         src="${project.img.mobile['1x']}" 
+        //         alt="${project.title}" 
+        //         width="320" 
+        //         height="180"
+        //     />
+        //     </picture>
 
-    <p class="projects-stack">${project.stack}</p>
-    <div class="projects-inner">
+        //     <p class="projects-stack">${project.stack}</p>
+        //     <div class="projects-inner">
+        //         <h3 class="projects-name">${project.title.toUpperCase()}</h3>
+        //         <a
+        //             class="projects-link"
+        //             href="${project.link}"
+        //             target="_blank"
+        //             rel="noopener noreferrer"
+        //         >
+        //             VISIT
+        //             <img
+        //             class="projects-icon"
+        //             src="/img/sprite-svg/icon-projects.svg"
+        //             alt="Arrow icon"
+        //             width="18"
+        //             height="18"
+        //             />
+        //         </a>
+        //     </div>
+
+        // `;
+
+        //             list.appendChild(li);
+        //         });
+
+        list.innerHTML = projects
+            .slice(0, visibleProjects)
+            .map(project => `
+    <li class="projects-item">
+      <picture>
+        <source media="(min-width: 1280px)" srcset="${project.img.desktop['1x']} 1x, ${project.img.desktop['2x']} 2x" />
+        <source media="(min-width: 768px)" srcset="${project.img.tablet['1x']} 1x, ${project.img.tablet['2x']} 2x" />
+        <source media="(max-width: 767px)" srcset="${project.img.mobile['1x']} 1x, ${project.img.mobile['2x']} 2x" />
+        <img
+          class="projects-image"
+          src="${project.img.mobile['1x']}"
+          alt="${project.title}"
+          width="320"
+          height="180"
+        />
+      </picture>
+      <p class="projects-stack">${project.stack}</p>
+      <div class="projects-inner">
         <h3 class="projects-name">${project.title.toUpperCase()}</h3>
         <a
-            class="projects-link"
-            href="${project.link}"
-            target="_blank"
-            rel="noopener noreferrer"
+          class="projects-link"
+          href="${project.link}"
+          target="_blank"
+          rel="noopener noreferrer"
         >
-            VISIT
-            <img
+          VISIT
+          <img
             class="projects-icon"
             src="/img/sprite-svg/icon-projects.svg"
             alt="Arrow icon"
             width="18"
             height="18"
-            />
+          />
         </a>
-    </div>
-
-`;
-
-            list.appendChild(li);
-        });
+      </div>
+    </li>
+  `)
+            .join('');
 
         const button = document.querySelector('[data-load-more]');
         if (visibleProjects >= projects.length) {
